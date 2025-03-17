6675d37596ad9bd093c8c6393db26d76dc379f05
fatima-khan712 <fatimakhanjadoon2005@gmail.com>
2025-03-17 08:02:49 -0700

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controller.withprinciple;
+package controller.withoutprinciple;
 
 /**
  *
  * @author GCA
  */
-public class Calculator {
+class Calculator {
     private int result;
 
     public Calculator() {
@@ -39,38 +39,14 @@ public class Calculator {
         }
     }
 }
-
-class CalculatorController {
-    private Calculator calculator; 
-
-    public CalculatorController(Calculator calculator) {
-        this.calculator = calculator;
-    }
-
-    public void performAddition(int num) {
-        calculator.add(num);
-    }
-
-    public void performSubtraction(int num) {
-        calculator.subtract(num);
-    }
-
-    public void performMultiplication(int num) {
-        calculator.multiply(num);
-    }
-
-    public void performDivision(int num) {
-        calculator.divide(num);
-    }
-}
-class WithController {
+class WithoutController {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();         
-        CalculatorController controller = new CalculatorController(calculator);  
+        Calculator calculator = new Calculator();  
 
-        controller.performAddition(10);   
-        controller.performSubtraction(5); 
-        controller.performMultiplication(2); 
-        controller.performDivision(0);    
+       
+        calculator.add(10);        
+        calculator.subtract(5);     
+        calculator.multiply(2);    
+        calculator.divide(0);      
     }
 }
